fad62c3a778bd3f775616a12a5a61b5799e3550c
reharsh67 <reharshdeshpande@gmail.com>
2023-01-21 16:39:48 +0530

    add authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,22 @@
 class SessionsController < ApplicationController
-    def new
-        
+  def new
+  end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:sucess] = "Welcome #{user.username}"
+      redirect_to root_path
+    else
+      flash.now[:error] = "wrong login details"
+      render "new"
     end
+
+    def destroy
+      session[:user_id] = nil
+      flash[:sucess] = "Logout sucessfuly"
+      redirect_to login_path
+    end
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:alert] = "Login to view this page"
+      redirect_to login_path
+    end
+  end
 end
