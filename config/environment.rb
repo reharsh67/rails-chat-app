52ae41d1cec3be7e11b4a7eeafa3b02fa0d943c1
reharshD <reharsh_deshpande@persistent.com>
2023-01-23 18:45:08 +0530

    remove for now

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+#config.action_controller.session = {
+#    :key          => '...',
+#    :secret       => '...',
+#    :expire_after => 1.minutes
+#  }
